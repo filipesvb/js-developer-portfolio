96e80e062b71854fdf934ac522de6862b14622b0
Filipe Vilas Bôas <fvb.boas@gmail.com>
2024-02-23 21:31:28 -0300

    made functions 'updateSoftSkills' and 'updateProfileData'

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,37 @@
 
+function updateProfileData(profileData) {
+
+    const photo = document.getElementById('profile.photo');
+    photo.alt = profileData.name;
+    photo.src = profileData.photo;
+
+    const name = document.getElementById('profile.name');
+    name.innerText = profileData.name;
+
+    const job = document.getElementById("profile.job");
+    job.innerText = profileData.job;
+
+    const location = document.getElementById("profile.location");
+    location.innerText = profileData.location;
+
+    const phone = document.getElementById("profile.phone");
+    phone.innerText = profileData.phone;
+    phone.href = `tel:${profileData.phone}`
+
+    const email = document.getElementById("profile.email");
+    email.innerText = profileData.email;
+    email.href = `mailto:${profileData.email}`
+
+}
+
+function updateSoftSkills(profileData) {
+    const softSkills = document.getElementById('profile.skills.softSkills');
+    
+    softSkills.innerHTML = profileData.skills.softSkills.map( (skill) => `<li> ${skill} </li>` ).join('');
+}
+
 (async () => {
-
     const profileData = await fetchProfileData();
-    console.log(profileData);
-
+    updateProfileData(profileData);
+    updateSoftSkills(profileData);
 })()
