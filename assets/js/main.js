fb3f682afd4ae1ff33f011bf5e4c46a4068b3747
Filipe Vilas Bôas <fvb.boas@gmail.com>
2024-02-23 23:02:54 -0300

    added function 'updatePortfolio'

--- assets/js/main.js
+++ assets/js/main.js
@@ -42,6 +42,19 @@ function updateLanguages(profileData) {
     languages.innerHTML = profileData.languages.map( (language) => `<li>${language}</li>`).join('');
 }
 
+function updatePortfolio(profileData) {
+    const portfolio = document.getElementById('profile.portfolio');
+    portfolio.innerHTML = profileData.portfolio.map((project) => {
+        return `
+            <li>
+                <h3 ${project.github ? 'class="github"' : ''}>${project.name}</h3>
+                <a href="${project.url}" target="_blank">${project.url}</a>
+            </li>
+        `
+    }).join('');
+    
+}
+
 (async () => {
     const profileData = await fetchProfileData();
     console.log(profileData);
@@ -49,4 +62,5 @@ function updateLanguages(profileData) {
     updateSoftSkills(profileData);
     updateHardSkills(profileData);
     updateLanguages(profileData);
+    updatePortfolio(profileData);
 })()
